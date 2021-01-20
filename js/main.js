b672c6c38f97b289b8af2e45aa2261068a629e56
komito1 <72906027+komito1@users.noreply.github.com>
2021-01-20 13:23:19 +0900

    0

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
-$("#8c").hide();
-$("#18c").hide();
-$("#28c").hide();
+$(window).load(function() {
+  $("#8c").hide();
+  $("#18c").hide();
+  $("#28c").hide();
 
-$("#c1").click(function(){
-  $("#8c").show();
-  $("#18c").hide();
-  $("#28c").hide();
-});
-$("#c2").click(function(){
-  $("#8c").hide();
-  $("#18c").show();
-  $("#28c").hide();
-});
-$("#c3").click(function(){
-  $("#8c").hide();
-  $("#18c").hide();
-  $("#28c").show();
+  $("#c1").click(function() {
+    $("#8c").show();
+    $("#18c").hide();
+    $("#28c").hide();
+  });
+  $("#c2").click(function() {
+    $("#8c").hide();
+    $("#18c").show();
+    $("#28c").hide();
+  });
+  $("#c3").click(function() {
+    $("#8c").hide();
+    $("#18c").hide();
+    $("#28c").show();
+  });
 });
