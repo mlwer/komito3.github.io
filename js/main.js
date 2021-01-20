2fd3b381ac63c764116b5145862e30dad289c5a2
komito1 <72906027+komito1@users.noreply.github.com>
2021-01-20 10:19:13 +0900

    0

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$("#8c").hide();
+$("#18c").hide();
+$("#28c").hide();
+
+$("#c1").click(function(){
+  $("#8c").show();
+  $("#18c").hide();
+  $("#28c").hide();
+});
+$("#c2").click(function(){
+  $("#8c").hide();
+  $("#18c").show();
+  $("#28c").hide();
+});
+$("#c3").click(function(){
+  $("#8c").hide();
+  $("#18c").hide();
+  $("#28c").show();
+});
